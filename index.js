7fdd0686cd0c48dea48b668bdaaf31eb0b0277d9
André Pena <andrerpena@gmail.com>
2015-12-11 19:12:11 -0200

    Adding redux-devtools

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './src/containers/App';
 import configureStore from './src/store/configureStore'
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 const store = configureStore();
 
 render(
-    <Provider store={store}>
-        <App/>
-    </Provider>, document.getElementById('root')
+    <div>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+        <DebugPanel top right bottom>
+            <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
+    </div>, document.getElementById('root')
 );
