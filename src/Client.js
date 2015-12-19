7df89c07b3fe9c97262476788cf0050b44de3753
André Pena <andrerpena@gmail.com>
2015-12-19 14:43:55 -0200

    Removing routes temporarily. I'll come back with them

--- src/Client.js
+++ src/Client.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Router } from 'react-router'
 import routes from './Routes';
+import App from './client/views/App';
 
 // styles
 import styles from './client/less/styles.less';
 
 const createBrowserHistory = require('history/lib/createBrowserHistory');
 ReactDom.render ((
-    <Router history={createBrowserHistory()}>
-        {routes}
-    </Router>
+    <App />
+    //<Router history={createBrowserHistory()}>
+    //    {routes}
+    //</Router>
 ),  document.getElementById('#app_container'));
