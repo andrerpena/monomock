028665f5c04a38caa5e96d86ae6879b1df0db1c3
André Pena <andrerpena@gmail.com>
2015-12-19 14:48:00 -0200

    Setting up Redux

--- src/Client.js
+++ src/Client.js
@@ -3,13 +3,19 @@ import ReactDom from 'react-dom';
 import { Router } from 'react-router'
 import routes from './Routes';
 import App from './client/views/App';
+import configureStore from './client/store/configureStore';
+import { Provider } from 'react-redux';
 
 // styles
 import styles from './client/less/styles.less';
 
+const store = configureStore();
+
 const createBrowserHistory = require('history/lib/createBrowserHistory');
 ReactDom.render ((
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
     //<Router history={createBrowserHistory()}>
     //    {routes}
     //</Router>
