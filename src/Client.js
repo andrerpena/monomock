1af3012a7618e4aff8f984cb5af7afd742614b7b
André Pena <andrerpena@gmail.com>
2015-12-19 14:52:16 -0200

    Temporarily remoing the router

--- src/Client.js
+++ src/Client.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Router } from 'react-router'
-import routes from './Routes';
 import App from './client/views/App';
 import configureStore from './client/store/configureStore';
 import { Provider } from 'react-redux';
@@ -11,12 +9,8 @@ import styles from './client/less/styles.less';
 
 const store = configureStore();
 
-const createBrowserHistory = require('history/lib/createBrowserHistory');
 ReactDom.render ((
     <Provider store={store}>
         <App />
     </Provider>
-    //<Router history={createBrowserHistory()}>
-    //    {routes}
-    //</Router>
 ),  document.getElementById('#app_container'));
