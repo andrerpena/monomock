119b52dbfbaab37827d7b08e8c92942a2e0e57b3
André Pena <andrerpena@gmail.com>
2016-01-21 21:17:09 -0200

    Finishing LESS to Sass conversion

--- src/Client.js
+++ src/Client.js
@@ -6,7 +6,8 @@ import { Provider } from 'react-redux';
 import DevTools from './client/DevTools';
 
 // styles
-import styles from './client/less/styles.less';
+import fontAwesome from 'font-awesome/less/font-awesome.less';
+import styles from './client/less/styles.scss';
 import sassTest from './client/less/test.scss';
 
 const store = configureStore();
